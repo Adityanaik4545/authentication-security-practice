1db5aca94a8f0dd3abea7de0fb308bd9439b61d0
Adityanaik4545 <adityank23@outlook.com>
2025-11-22 17:48:56 +0530

    made use of env variables, learned about Oauth by implementing google signIn

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import bycrpt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth20";
+import env from "dotenv";
+
+env.config()
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "Aditya@123",
+  user: process.env.db_user,
+  host: process.env.db_host,
+  database: process.env.db_database,
+  password: process.env.db_password,
   port: 5432,
 
 })
@@ -20,7 +24,7 @@ const app = express();
 const port = 3000;
 
 app.use(session({
-  secret: "Topsecret",
+  secret: process.env.session_secret,
   saveUninitialized: false,
   resave: false,
   cookie: {
@@ -56,6 +60,22 @@ app.get("/secrets", (req, res) => {
   }
 });
 
+app.get("/auth/google", passport.authenticate("google",{
+  scope: ["profile", "email"]
+}))
+
+app.get("/auth/google/secrets", passport.authenticate("google",{
+  successRedirect: "/secrets",
+  failureRedirect: "/login"
+}))
+
+app.get("/logout", (req, res)=>{
+    req.logout((err)=>{
+      if(err) console.log(err);
+      res.redirect("/")
+    })
+})
+
 app.post("/login", passport.authenticate("local",{
   successRedirect: "/secrets",
   failureRedirect: "/login"
@@ -76,8 +96,13 @@ try {
         if (err) {
           res.send(" register_ something went wrong!", err)
         }
-        await db.query("insert into users (email, password) values($1,$2)", [uEmail, hash]);
-        res.render("secrets.ejs");
+        const result = await db.query("insert into users (email, password) values($1,$2) returning *", [uEmail, hash]);
+        const user = result.rows[0]
+        req.login(user, (err)=>{
+          console.log(err);
+          res.redirect("/secrets")
+          
+        })
       })
     }
 } catch (error) {
@@ -114,6 +139,30 @@ passport.use(new Strategy(async function verify(username, password, cb) {
     
   }
 }))
+
+passport.use("google", new GoogleStrategy(
+  {
+    clientID: process.env.client_id,
+    clientSecret: process.env.client_secret,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  }, async(accessToken, refreshToken, profile, cb)=>{
+      console.log("google email ->",profile.emails[0]);
+      try {
+        const result = await db.query("select * from users where email = $1", [profile.emails[0].value]);
+        if (result.rows.length === 0) {
+          const newUser = await db.query("insert into users (email, password) values($1, $2)",[profile.emails[0].value, "google"])
+          cb(null, newUser.rows[0]);
+        } else{
+          cb(null, result.rows[0])
+        }
+        
+      } catch (error) {
+        return cb(error)
+      }
+      
+  }
+))
 
 passport.serializeUser((user, cb)=>{
   cb(null, user.id);
